ad4ef0b2d51c9e9c738451bb09653fe41abac010
bn-dhiraj-chaudhary <dhiraj.chaudhary@bridgenext.com>
2026-09-04 17:30:57 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -4,19 +4,8 @@ node {
         POLARIS__TOKEN = credentials('POLARIS_TOKEN')
     }
     
-        stage('Polaris Black Duck Security Scan') {
-            steps {
-                security_scan(
-                    product: 'polaris',
-                    polaris_server_url: POLARIS_URL,
-                    polaris_access_token: POLARIS__TOKEN,
-                    polaris_application_name: 'BN-cop-test-javascript-app',
-                    polaris_project_name: 'bn-dhiraj-chaudhary/BN-cop-test-javascript',
-                    polaris_branch_name: 'main',
-                    polaris_assessment_types: 'SAST,SCA'
-                )
-            }
-        }
+       
+        
         
     }
 }
